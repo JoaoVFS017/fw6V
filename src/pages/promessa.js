9a7bec278b207c2ca6861ad52b5a0e94cc996aa3
JoaoVFS017 <119964116+JoaoVFS017@users.noreply.github.com>
2024-10-29 18:03:21 +0000

    aula 29-10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -1,14 +1,18 @@
 import { Container } from "react-bootstrap";
 import Menu from "./components/navbar";
 import Footer from "./components/footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Promessa(){
     const [divida, setDivida] = useState("")
+    useEffect(() =>{
+
+  
     setTimeout(() => {
         document.title = "Minha promessa"
         setDivida("da minha vida")
     }, 3000);
+  })
     return <><Menu/>
     <Container>
         Promessas {(divida==="" ? " carregando...": divida)}
