b4bca3b418ac489037223fecd8dc0e900e21c8cb
JoaoVFS017 <119964116+JoaoVFS017@users.noreply.github.com>
2024-10-29 18:28:16 +0000

    aula dia 29/10

--- src/pages/promessa.js
+++ src/pages/promessa.js
@@ -6,17 +6,20 @@ import { useEffect, useState } from "react";
 export default function Promessa(){
     const [divida, setDivida] = useState("")
     useEffect(() =>{
-
-  
-    setTimeout(() => {
+        setTimeout(() => {
         document.title = "Minha promessa"
-        setDivida("da minha vida")
+        setDivida(saoLonguinho("da minha vida"))
     }, 3000);
   })
     return <><Menu/>
     <Container>
-        Promessas {(divida==="" ? " carregando...": divida)}
+        Promessas {(divida==="" ? <img src="gif1.gif" width={40} height={60}/>: divida)}
     </Container>
     <Footer/>
     </>
+}
+function saoLonguinho(texto) {
+    setTimeout(()=>{
+        return (texto)
+    }, 2000)
 }
